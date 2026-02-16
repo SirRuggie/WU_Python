6d316a5358b390ffe99b2aeb38dd88f54f69177e
Ruggie <shaun.m.bridges@gmail.com>
2026-02-16 13:00:06 -0500

    fix: chocolate request missing comma

--- extensions/commands/fwa/chocolate.py
+++ extensions/commands/fwa/chocolate.py
@@ -159,7 +159,7 @@ class ChocolateCommand(
                             )
                         ]
                     ),
-                    Media(items=[MediaItem(media="assets/Gold_Footer.png")])
+                    Media(items=[MediaItem(media="assets/Gold_Footer.png")]),
                     Separator(divider=True),
                     Text(content=f"-# Requested by <@{ctx.member.id}>"),
                 ]
